b9b3d36dabcb9e400ad052af76755428b0601ad7
Brijamohan <115333861+Sexykoder@users.noreply.github.com>
2024-06-13 11:47:46 +0530

    Update LC104.cpp

---
+++ Binary Tree/LC104.cpp
@@ -0,0 +1,15 @@
+
+
+
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        int lh = maxDepth(root -> left);
+        int rh = maxDepth(root -> right);
+
+        return 1 + max(rh,lh);
+    }
+};
